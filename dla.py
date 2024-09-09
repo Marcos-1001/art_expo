ad47e7935d4d7c0217ab3aa1c55c12067eec0b46
Marcos <marcos.ayala@utec.edu.pe>
2024-09-08 23:22:07 -0500

    changes in  dla

--- dla.py
+++ dla.py
@@ -62,9 +62,42 @@ def diffusion_limited_agg(image_size: int, seed : tuple ,image: Image):
                     for z in range(j-3, j+3):
                         if not is_limit(k, z, image_size) :
                             image.putpixel((k, z), (255, 255, 255))
+    return tree
+def map_image_to_tree(image: Image ) -> None:
+    tree = [[0 for i in range(image.height)] for j in range(image.width)]
+    for i in range(image.width):
+        for j in range(image.height):
+            if image.getpixel((i, j)) == (255, 255, 255):
+                tree[i][j] = 1
+    return tree
+
+
+def gaussian_color() -> tuple:
+    random = np.random.normal(128, 20, 3)
+    return  (int(random[0]), int(random[1]), int(random[2]))
+
+def mix_colors(color1 : tuple, color2 : tuple) -> tuple:
+    final  = np.clip((np.array(color1)*np.array(color2)) , 0, 255).tolist()
+    #print(final)
+    return (int(final[0]), int(final[1]), int(final[2]))
+
+def gradient_color(image,tree, seed, color) -> tuple:
+    queue = [(seed, color, 0)]
+    limits = len(tree)
+    while queue:
+        ( (x, y) , color, step) = queue.pop(0)
+        for dx, dy in all_directions:
+            if not is_limit(x+dx, y+dy, limits) and tree[x+dx][y+dy] == 1:
+
+                tree[x+dx][y+dy] = 2
+                if step % 45 == 0: 
+                    queue.append(((x+dx, y+dy), mix_colors(color, (1.075, 1.0015, 1.0009)), step+1))
+                    #print("here")
+                else: 
+                    queue.append(((x+dx, y+dy), color, step+1))
+                image.putpixel((x+dx, y+dy), color)
+
     
-
-
     
 
 
@@ -72,7 +105,11 @@ def diffusion_limited_agg(image_size: int, seed : tuple ,image: Image):
 
 if __name__ == "__main__":
     image_size = 1000 
-    image = Image.new("RGB", (image_size, image_size), (0, 0, 0))
+    #image = Image.new("RGB", (image_size, image_size), (0, 0, 0))
     seed = (image_size//2, image_size//2)
-    diffusion_limited_agg(image_size, seed, image)
-    image.save("dla_2.png")
+    
+    image = Image.open("dla_3.png")
+    tree = map_image_to_tree(image=image)
+    #tree = diffusion_limited_agg(image_size, seed, image)
+    gradient_color(image, tree, seed, (100, 100, 100))
+    image.save("dla_4.png")
